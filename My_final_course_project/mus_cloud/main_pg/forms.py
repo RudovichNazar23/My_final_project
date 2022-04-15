3e9cf85b6abfe6e60a8afe5ad6f9083feb374f5a
RudovichNazar23 <n.rudovich2005@gmail.com>
2022-04-15 18:42:04 +0300

    added new

--- My_final_course_project/mus_cloud/main_pg/forms.py
+++ My_final_course_project/mus_cloud/main_pg/forms.py
@@ -10,3 +10,6 @@ class SongForm(forms.Form):
 class AlbumForm(forms.Form):
     name_album = forms.CharField(max_length=100)
     album = forms.FileField(widget=forms.ClearableFileInput(attrs={"multiple": True}))
+
+class SearchUserForm(forms.Form):
+    username = forms.CharField(max_length=100)
--- My_final_course_project/mus_cloud/mus_cloud/settings.py
+++ My_final_course_project/mus_cloud/mus_cloud/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'main_pg'
+    'main_pg',
 ]
 
 MIDDLEWARE = [
